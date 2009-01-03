3e98345ec26e6e7388c078f011693417ac4b459b
Juozas Gaigalas <juozas@hobo23.(none)>
2009-01-03 14:52:13 -0800

    extented greencloth test to generate and check for realistic edit section links

--- lib/greencloth/run-tests.rb
+++ lib/greencloth/run-tests.rb
@@ -6,6 +6,10 @@ require File.join(File.expand_path(File.dirname(__FILE__), '../extension/string'
 
 require 'yaml'
 
+def edit_section_link(section_title, section_index)
+  %Q{<a href="/edit?section=#{section_index}"><img src="http://localhost:3000/images/actions/pencil.png" alt="" />edit section</a>}
+end
+
 i = 0
 files = ARGV[0] || Dir["tests/*.yml"]
 files.each do |testfile|
@@ -15,7 +19,13 @@ files.each do |testfile|
     in_markup = doc['in']
     out_markup = doc['out'] || doc['html']
     if in_markup and out_markup
-      html = GreenCloth.new( in_markup ).to_html      
+      greencloth = GreenCloth.new( in_markup )
+      
+      # generate section edit links for section.yml
+      if testfile =~ /sections\.yml/
+        greencloth.on_edit_section_link { |title, i| edit_section_link(title, i) }
+      end
+      html = greencloth.to_html      
       html.gsub!( /\n+/, "\n" )
       out_markup.gsub!( /\n+/, "\n" )
       if html == out_markup
